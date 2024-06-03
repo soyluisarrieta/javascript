4f94ad1fa1eef79ed723227de57cb3f3ac30d551
Luis Arrieta <luisarrieta796@gmail.com>
2024-06-03 18:41:16 -0500

    Change directories and create utils

---
+++ utils/forceFullscreen.js
@@ -0,0 +1,25 @@
+import sizes from './getSizes.js'
+
+// Observar evento cuando cambia el fullscreen
+document.addEventListener('fullscreenchange', function () {
+  if (document.fullscreenElement) {
+    console.log("En pantalla completa", sizes())
+  } else {
+    console.log("No está en pantalla completa", sizes())
+  }
+})
+
+// Activar fullscreen cuando presiona clic
+document.addEventListener('click', function () {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen().catch(err => {
+      console.log(`Error al intentar entrar en modo de pantalla completa: ${err.message} (${err.name})`)
+    })
+  } else {
+    /* Salir de pantalla completa
+    document.exitFullscreen().catch(err => {
+        console.log(`Error al intentar salir del modo de pantalla completa: ${err.message} (${err.name})`)
+    })
+    */
+  }
+})
